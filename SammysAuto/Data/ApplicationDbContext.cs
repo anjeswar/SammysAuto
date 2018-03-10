caeecc76822897a47599e291be7b7f82fcf71264
Prasanthi <mnrrau@gmail.com>
2018-03-10 20:56:03 +0530

    Edit, Details and Delete operations

--- SammysAuto/Data/ApplicationDbContext.cs
+++ SammysAuto/Data/ApplicationDbContext.cs
@@ -14,7 +14,7 @@ namespace SammysAuto.Data
             : base(options)
         {
         }
-
+        public DbSet<ServiceType> ServiceTypes { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
